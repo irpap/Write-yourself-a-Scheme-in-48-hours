867fc61c52670d911202e196851b526cbadcc5cc
Irene Papakonstantinou <eirhnh@gmail.com>
2013-03-28 21:00:34 +0000

    parse whitespace followed by special characters

---
+++ chapter2/exercise1.hs
@@ -0,0 +1,18 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err ->"No match: " ++ show err
+    Right val -> "Found value"
+
+main :: IO()
+main = do 
+    args <- getArgs
+    putStrLn(readExpr $ args !! 0)
