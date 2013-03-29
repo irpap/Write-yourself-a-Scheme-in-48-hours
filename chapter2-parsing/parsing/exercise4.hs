513d57e235c3731eee7e29717a5174735a073d44
Irene Papakonstantinou <eirhnh@gmail.com>
2013-03-29 22:36:24 +0000

    parse numbers in different bases. deal with escaped characters in strings

---
+++ chapter2-parsing/parsing/exercise4.hs
@@ -0,0 +1,84 @@
+import Data.Char
+import Control.Monad
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+data LispVal = Atom String
+                | List [LispVal]
+                | DottedList [LispVal] LispVal
+                | Number Integer
+                | String String
+                | Bool Bool
+                
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+escapedChar :: Parser Char
+escapedChar = char '\\' >> oneOf("\"nrt\\") >>= \c -> 
+                            return $ case c of
+                                    '\\' -> '\\'
+                                    'n' -> '\n'
+                                    'r' -> '\r'
+                                    't' -> '\t'
+
+parseString :: Parser LispVal
+parseString = do  
+                  char '"'
+                  x <- many (noneOf "\"" <|> escapedChar)
+                  char '"'
+                  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+                first <- letter <|> symbol
+                rest <- many (letter <|> digit <|> symbol)
+                let atom = first:rest
+                return $ case atom of
+                            "#t" -> Bool True
+                            "#f" -> Bool False
+                            _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = readPlainNumber <|> parseNumberInBase
+
+readPlainNumber:: Parser LispVal
+readPlainNumber = do
+                    d <- many1 digit
+                    return $ Number . read $ d
+
+parseNumberInBase :: Parser LispVal
+parseNumberInBase = char '#' >> 
+                    ((char 'd' >> readPlainNumber)
+                     <|> (char 'b' >> readBinaryNumber)
+                     <|> (char 'o' >> readOctalNumber)
+                     <|> (char 'x' >> readHexNumber))
+
+readBinaryNumber = readNumberInBase "01" 2 
+readOctalNumber = readNumberInBase "01234567" 8
+readHexNumber = readNumberInBase "0123456789abcdefABCEDF" 16
+
+readNumberInBase :: String -> Integer -> Parser LispVal
+readNumberInBase digits base = do 
+                    d <- many (oneOf (digits))
+                    return $ Number $ toDecimal d base
+
+toDecimal :: String -> Integer -> Integer
+toDecimal s base  = ( foldl1 ((+) . (* base)) ( map  (toInteger . digitToInt) s ))
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom 
+            <|> parseString
+            <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err ->"No match: " ++ show err
+    Right val -> "Found value"
+
+main :: IO()
+main = do 
+    args <- getArgs
+    putStrLn(readExpr $ args !! 0)
