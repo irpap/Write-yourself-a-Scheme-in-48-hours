89c3d0aa507c1cc1baae4f7a81ab9fa13838e90e
Irene Papakonstantinou <eirhnh@gmail.com>
2013-04-07 17:45:06 +0100

    Parse lists, dotted lists still not working

---
+++ chapter2-parsing/recursive-parser/SchemeParser.hs
@@ -0,0 +1,142 @@
+module SchemeParser (LispVal (..), readExpr) where
+
+import Data.Char
+import Control.Monad
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Numeric
+import Data.Ratio
+import Data.Complex
+
+data LispVal = Atom String
+                | List [LispVal]
+                | DottedList [LispVal] LispVal
+                | Number Integer
+                | Float Double
+                | Complex  (Complex Double)
+                | Rational Rational
+                | String String
+                | Bool Bool
+                | Char Char
+                deriving (Eq, Show)
+
+parseExpr :: Parser LispVal
+parseExpr = try parseComplexNumber
+            <|> try parseFloat 
+            <|> try parseRationalNumber
+            <|> try parseNumber
+            <|> try parseChar
+            <|> parseAtom
+            <|> parseString
+            <|> parseQuoted
+            <|> do  char '('
+                    x <- try parseDottedList <|> parseList
+                    char ')'
+                    return x
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err ->"No match: " ++ show err
+    Right val -> show val
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+        head <- endBy parseExpr spaces
+        tail <- char '.' >> spaces >> parseExpr
+        return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+        char '\''
+        x <- parseExpr
+        return $ List [Atom "quote", x]
+
+parseComplexNumber :: Parser LispVal
+parseComplexNumber = do realPart <- fmap toDouble $ (try parseFloat) <|> readPlainNumber
+                        char '+'
+                        imaginaryPart <- fmap toDouble $ (try parseFloat) <|> readPlainNumber
+                        char 'i'
+                        return $ Complex (realPart :+ imaginaryPart)
+                            where toDouble (Float x) = x
+                                  toDouble (Number x) = fromInteger x :: Double 
+                                                    
+
+parseRationalNumber :: Parser LispVal
+parseRationalNumber = do numerator <- many digit
+                         char '/'
+                         denominator <- many digit
+                         return $ Rational (read (numerator ++ "%" ++ denominator) :: Rational)
+                    
+parseFloat :: Parser LispVal
+parseFloat = do whole <- many digit
+                char '.'
+                decimal <- many digit
+                return $ Float (read (whole ++ "." ++ decimal) :: Double)
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest
+               return $ case atom of
+                           "#t" -> Bool True
+                           "#f" -> Bool False
+                           _    -> Atom atom
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseChar :: Parser LispVal
+parseChar = do string "#\\"
+               c <- many1 letter
+               return $ case map toLower c of
+                   "newline" -> Char '\n'
+                   "space" -> Char ' '
+                   [x] -> Char x
+
+escapedChar :: Parser Char
+escapedChar = char '\\' >> oneOf("\"nrt\\") >>= \c -> 
+                            return $ case c of
+                                    '\\' -> '\\'
+                                    'n' -> '\n'
+                                    'r' -> '\r'
+                                    't' -> '\t'
+
+parseString :: Parser LispVal
+parseString = do  char '"'
+                  x <- many (noneOf "\"" <|> escapedChar)
+                  char '"'
+                  return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = readPlainNumber <|> parseRadixNumber
+
+readPlainNumber:: Parser LispVal
+readPlainNumber = do
+                    d <- many1 digit
+                    return $ Number . read $ d
+
+parseRadixNumber :: Parser LispVal
+parseRadixNumber = char '#' >> 
+                    ((char 'd' >> readPlainNumber)
+                     <|> (char 'b' >> readBinaryNumber)
+                     <|> (char 'o' >> readOctalNumber)
+                     <|> (char 'x' >> readHexNumber))
+
+readBinaryNumber = readNumberInBase "01" 2 
+readOctalNumber = readNumberInBase "01234567" 8
+readHexNumber = readNumberInBase "0123456789abcdefABCEDF" 16
+
+readNumberInBase :: String -> Integer -> Parser LispVal
+readNumberInBase digits base = do 
+                    d <- many (oneOf (digits))
+                    return $ Number $ toDecimal base d
+
+toDecimal :: Integer -> String -> Integer
+toDecimal base s = foldl1 ((+) . (* base)) $ map toNumber s
+                    where toNumber  =  (toInteger . digitToInt)  
+
---
+++ chapter2-parsing/recursive-parser/Test.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Data.Char
+import Data.Complex
+import SchemeParser
+import Test.HUnit
+
+main = runTestTT $ test tests
+
+tests :: [Test]
+tests = [
+        TestCase $ assertEqual "tests parsing a complex number" (readExpr "3.32+5i") "Complex (3.32 :+ 5.0)",
+        TestCase $ assertEqual "tests parsing a rational number" (readExpr "6/2") "Rational (3 % 1)",
+        TestCase $ assertEqual "tests parsing a list" (readExpr "(1 2 3)") "List [Number 1,Number 2,Number 3]"
+         ]
+         --TestCase $ assertEqual "tests parsing a dotted list" (readExpr "(1 . 2)" "DottedList List[Number 1], Number 2"
