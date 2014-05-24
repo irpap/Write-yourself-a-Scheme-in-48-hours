3b2bcd990d55164b7d153a0a157d792725799932
Irene Papakonstantinou <irp@fb.com>
2014-05-24 15:37:03 +0100

    Use TestList

--- chapter2-parsing/parsing/test.hs
+++ chapter2-parsing/parsing/test.hs
@@ -7,7 +7,7 @@ import Test.HUnit
 
 main = runTestTT $ test tests
 
-tests :: [Test]
-tests = [
-    TestCase $ assertEqual "tests parsing a complex number" (readExpr "3.32+5i") "Complex (3.32 :+ 5.0)",
-    TestCase $ assertEqual "tests parsing a rational number" (readExpr "6/2") "Rational (3 % 1)"]
+tests :: Test
+tests = TestList $ map TestCase [
+    assertEqual "tests parsing a complex number" (readExpr "3.32+5i") "Complex (3.32 :+ 5.0)",
+    assertEqual "tests parsing a rational number" (readExpr "6/2") "Rational (3 % 1)"]
--- chapter2-parsing/recursive-parser/Test.hs
+++ chapter2-parsing/recursive-parser/Test.hs
@@ -7,10 +7,10 @@ import Test.HUnit
 
 main = runTestTT $ test tests
 
-tests :: [Test]
-tests = [
-        TestCase $ assertEqual "tests parsing a complex number" (readExpr "3.32+5i") "Complex (3.32 :+ 5.0)",
-        TestCase $ assertEqual "tests parsing a rational number" (readExpr "6/2") "Rational (3 % 1)",
-        TestCase $ assertEqual "tests parsing a list" (readExpr "(1 2 3)") "List [Number 1,Number 2,Number 3]",
-        TestCase $ assertEqual "tests parsing a dotted list" (readExpr "(a (dotted . list) test)") "List [Atom \"a\",DottedList [Atom \"dotted\"] (Atom \"list\"),Atom \"test\"]"
+tests :: Test
+tests = TestList $ map TestCase [
+         assertEqual "tests parsing a complex number" (readExpr "3.32+5i") "Complex (3.32 :+ 5.0)",
+         assertEqual "tests parsing a rational number" (readExpr "6/2") "Rational (3 % 1)",
+         assertEqual "tests parsing a list" (readExpr "(1 2 3)") "List [Number 1,Number 2,Number 3]",
+         assertEqual "tests parsing a dotted list" (readExpr "(a (dotted . list) test)") "List [Atom \"a\",DottedList [Atom \"dotted\"] (Atom \"list\"),Atom \"test\"]"
         ]
